9eaac584c4550c4ef556f19e7c06531ba003595b
dsiders <noreply>
2015-05-29 14:10:16 +0000

    Add component Legend

---
+++ ui/legend.reel/legend.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/legend.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Legend
+ * @extends Component
+ */
+exports.Legend = Component.specialize(/** @lends Legend# */ {
+    constructor: {
+        value: function Legend() {
+            this.super();
+        }
+    }
+});
